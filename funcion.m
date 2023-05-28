87947becca8571055c0111aa08a12526e4d73137
XabiBlaz <132968827+XabiBlaz@users.noreply.github.com>
2023-05-29 01:28:57 +0200

    Delete funcion.m

--- funcion.m
+++
@@ -1,7 +0,0 @@
-function [y] = funcion(r)
-%FUNCION2D con la entrada de la matriz S y la posición actual te da la
-%respuesta del valor de la funcion
-S=10^-4*[2,1;1,1];
-y=-100*exp(-r'*S*r);
-end
-
