f99fb9ee155fce5de9f0d74f2ff2eb602c2b0858
XabiBlaz <132968827+XabiBlaz@users.noreply.github.com>
2023-05-29 01:28:39 +0200

    Delete funcion_positiva.m

--- funcion_positiva.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = funcion_positiva(inputArg1,inputArg2)
-%FUNCION_POSITIVA Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
